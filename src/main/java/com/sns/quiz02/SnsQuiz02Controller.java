dc3f5ed51d54fd852cef8f5de335c0c0654858f8
학원 <test@test.com>
2023-10-27 23:28:16 +0900

    10/27수업 자료

---
+++ src/main/java/com/sns/quiz02/SnsQuiz02Controller.java
@@ -0,0 +1,11 @@
+package com.sns.quiz02;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SnsQuiz02Controller {
+	public String quiz02_1(){
+		return"quiz02/addUser";
+	}
+	
+}
